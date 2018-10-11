99a115f037653b946eeaa97a02e4a1c5417fe1b6
Ian Macdonald <ian@caliban.org>
2018-10-11 14:20:14 +0200

    Never call max77705_rgb_ramp() when led_enable_fade disabled.
    For behaviour true to stock, when led_enable_fade is disabled we no
    longer call max77705_rgb_ramp() anywhere that it was not already called
    in the original source. In practice, this means only in the case of the
    POWERING state.
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/leds/leds-max77705-rgb.c
+++ drivers/leds/leds-max77705-rgb.c
@@ -295,8 +295,8 @@ static int max77705_rgb_ramp(struct device *dev, int ramp_up, int ramp_down)
 
 	pr_info("leds-max77705-rgb: %s\n", __func__);
 
-	led_fade_time_up = led_enable_fade ? led_fade_time_up : LED_RMP_TIME;
-	led_fade_time_down = led_enable_fade ? led_fade_time_down : LED_RMP_TIME;
+	if (! led_enable_fade)
+		led_fade_time_up = led_fade_time_down = LED_RMP_TIME;
 
 	if (ramp_up > led_fade_time_up)
 		ramp_up = (ramp_up - led_fade_time_up) * 2 + led_fade_time_up;
@@ -586,26 +586,43 @@ static ssize_t store_max77705_rgb_pattern(struct device *dev,
 		max77705_rgb_set_state(&max77705_rgb->led[RED], led_dynamic_current, LED_ALWAYS_ON);
 		break;
 	case CHARGING_ERR:
-		max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
-		max77705_rgb_blink(dev, 500, 500);
+		if (led_enable_fade) {
+			max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
+			max77705_rgb_blink(dev, led_fade_time_up, 500);
+		} else {
+			max77705_rgb_blink(dev, 500, 500);
+		}
 		max77705_rgb_set_state(&max77705_rgb->led[RED], led_dynamic_current, LED_BLINK);
 		break;
 	case MISSED_NOTI:
-		max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
-		max77705_rgb_blink(dev, 500, 5000);
+		if (led_enable_fade) {
+			max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
+			max77705_rgb_blink(dev, led_fade_time_up, 5000);
+		} else {
+			max77705_rgb_blink(dev, 500, 5000);
+		}
 		max77705_rgb_set_state(&max77705_rgb->led[BLUE], led_dynamic_current, LED_BLINK);
 		break;
 	case LOW_BATTERY:
-		max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
-		max77705_rgb_blink(dev, 500, 5000);
+		if (led_enable_fade) {
+			max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
+			max77705_rgb_blink(dev, led_fade_time_up, 5000);
+		} else {
+			max77705_rgb_blink(dev, 500, 5000);
+		}
 		max77705_rgb_set_state(&max77705_rgb->led[RED], led_dynamic_current, LED_BLINK);
 		break;
 	case FULLY_CHARGED:
 		max77705_rgb_set_state(&max77705_rgb->led[GREEN], led_dynamic_current, LED_ALWAYS_ON);
 		break;
 	case POWERING:
-		max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
-		max77705_rgb_blink(dev, 200, 200);
+		if (led_enable_fade) {
+			max77705_rgb_ramp(dev, led_fade_time_up, led_fade_time_down);
+			max77705_rgb_blink(dev, led_fade_time_up, 200);
+		} else {
+			max77705_rgb_ramp(dev, LED_RMP_TIME, LED_RMP_TIME);
+			max77705_rgb_blink(dev, 200, 200);
+		}
 		max77705_rgb_set_state(&max77705_rgb->led[BLUE], led_dynamic_current, LED_ALWAYS_ON);
 		max77705_rgb_set_state(&max77705_rgb->led[GREEN], led_dynamic_current, LED_BLINK);
 		break;
@@ -910,7 +927,7 @@ static ssize_t led_fade_store(struct device *dev,
 
 	retval = sscanf(buf, "%1d", &enabled);
 
-	if (retval != 0 && (enabled == 0 || enabled == 1))
+	if (retval && (enabled == 0 || enabled == 1))
 		led_enable_fade = enabled;
 
 	return count;
@@ -934,7 +951,7 @@ static ssize_t led_fade_time_up_store(struct device *dev,
 
 	retval = sscanf(buf, "%d", &val);
 
-	if (retval != 0 && val >= 100 && val <= 4000)
+	if (retval && val >= 100 && val <= 4000)
 		led_fade_time_up = val;
 
 	return count;
@@ -958,7 +975,7 @@ static ssize_t led_fade_time_down_store(struct device *dev,
 
 	retval = sscanf(buf, "%d", &val);
 
-	if (retval != 0 && val >= 100 && val <= 4000)
+	if (retval && val >= 100 && val <= 4000)
 		led_fade_time_down = val;
 
 	return count;
