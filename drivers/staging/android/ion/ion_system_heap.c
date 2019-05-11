790a4a38e1a171f066a1dde4f5d11d9e8b401444
djb77 <dwayne.bakewell@gmail.com>
2019-05-12 02:55:09 +1000

    ion: Change low_order_gfp_flags to high_order_gfp_flags
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -367,8 +367,8 @@ static int ion_system_heap_create_pools(struct ion_page_pool **pools,
 		struct ion_page_pool *pool;
 		gfp_t gfp_flags = low_order_gfp_flags;
 
-		if (orders[i] < 4)
-			gfp_flags = low_order_gfp_flags;
+		if (orders[i] > 4)
+			gfp_flags = high_order_gfp_flags;
 
 		pool = ion_page_pool_create(gfp_flags, orders[i], cached);
 		if (!pool)
