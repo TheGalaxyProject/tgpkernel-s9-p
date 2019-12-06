f22dd26e8a4830b0e99b75b56ce6303144648ce6
djb77 <dwayne.bakewell@gmail.com>
2019-12-07 00:08:38 +1100

    REVERT: lib/genalloc.c: fix allocation of aligned buffer from non-aligned chunk
    This reverts commit bd5d9cd97ca55bba497a446f7a5d6a47b00869d5
    because it was producing compiling errors
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -312,7 +312,7 @@ unsigned long gen_pool_alloc_algo(struct gen_pool *pool, size_t size,
 		end_bit = chunk_size(chunk) >> order;
 retry:
 		start_bit = algo(chunk->bits, end_bit, start_bit,
-				 nbits, data, pool, chunk->start_addr);
+				 nbits, data, pool);
 		if (start_bit >= end_bit)
 			continue;
 		remain = bitmap_set_ll(chunk->bits, start_bit, nbits);
@@ -526,7 +526,7 @@ EXPORT_SYMBOL(gen_pool_set_algo);
  */
 unsigned long gen_pool_first_fit(unsigned long *map, unsigned long size,
 		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool, unsigned long start_addr)
+		struct gen_pool *pool)
 {
 	return bitmap_find_next_zero_area(map, size, start, nr, 0);
 }
@@ -544,19 +544,16 @@ EXPORT_SYMBOL(gen_pool_first_fit);
  */
 unsigned long gen_pool_first_fit_align(unsigned long *map, unsigned long size,
 		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool, unsigned long start_addr)
+		struct gen_pool *pool)
 {
 	struct genpool_data_align *alignment;
-	unsigned long align_mask, align_off;
+	unsigned long align_mask;
 	int order;
 
 	alignment = data;
 	order = pool->min_alloc_order;
 	align_mask = ((alignment->align + (1UL << order) - 1) >> order) - 1;
-	align_off = (start_addr & (alignment->align - 1)) >> order;
-
-	return bitmap_find_next_zero_area_off(map, size, start, nr,
-					      align_mask, align_off);
+	return bitmap_find_next_zero_area(map, size, start, nr, align_mask);
 }
 EXPORT_SYMBOL(gen_pool_first_fit_align);
 
@@ -571,7 +568,7 @@ EXPORT_SYMBOL(gen_pool_first_fit_align);
  */
 unsigned long gen_pool_fixed_alloc(unsigned long *map, unsigned long size,
 		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool, unsigned long start_addr)
+		struct gen_pool *pool)
 {
 	struct genpool_data_fixed *fixed_data;
 	int order;
@@ -605,8 +602,7 @@ EXPORT_SYMBOL(gen_pool_fixed_alloc);
  */
 unsigned long gen_pool_first_fit_order_align(unsigned long *map,
 		unsigned long size, unsigned long start,
-		unsigned int nr, void *data, struct gen_pool *pool,
-		unsigned long start_addr)
+		unsigned int nr, void *data, struct gen_pool *pool)
 {
 	unsigned long align_mask = roundup_pow_of_two(nr) - 1;
 
@@ -629,7 +625,7 @@ EXPORT_SYMBOL(gen_pool_first_fit_order_align);
  */
 unsigned long gen_pool_best_fit(unsigned long *map, unsigned long size,
 		unsigned long start, unsigned int nr, void *data,
-		struct gen_pool *pool, unsigned long start_addr)
+		struct gen_pool *pool)
 {
 	unsigned long start_bit = size;
 	unsigned long len = size + 1;
